e824f80a551eba70b9ea2e307fc507e5a28c0589
Akash <aakashkumbhare@gmail.com>
2023-03-21 21:23:09 +0530

    This is comment in index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+//this is comment in indes
   <React.StrictMode>
     <App />
   </React.StrictMode>
